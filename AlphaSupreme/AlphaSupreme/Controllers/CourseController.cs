b7924e1eddf8dcfd9a33f1c8dfdc644701bb9659
raju197 <42387398+raju197@users.noreply.github.com>
2019-08-08 14:12:38 +0530

    jkdjkjgkgj

---
+++ AlphaSupreme/AlphaSupreme/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlphaSupreme.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SearchCourse(string name) {
+
+            return View();
+        }
+    }
+}
